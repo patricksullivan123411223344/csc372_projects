49c3562cc16bc5f8b8e290bbd4fbe0e9fc1e0dd2
patricksullivan123411223344 <patrick.sullivan@uri.edu>
2026-02-02 20:39:57 -0500

    Finished my DOM testing

---
+++ DOM-EVENTS-PRACTICE/dynamic.js
@@ -0,0 +1,21 @@
+// 1. State (JS Memory)
+const names = ["Patrick", "Johnny", "Tim"];
+let index = 0;
+
+const namesTarget = document.getElementById("nameTarget");
+const button = document.getElementById("changeNameBtn")
+
+button.addEventListener("click", () => {
+    index++;
+
+    if (index === names.length) {
+        index = 0;
+    }
+
+    namesTarget.textContent = names[index];
+});
+
+
+
+
+
