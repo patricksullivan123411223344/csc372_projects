dcf9d969e7abead2528ead51f880e50906c5e4cf
patricksullivan123411223344 <patrick.sullivan@uri.edu>
2026-02-08 13:45:50 -0500

    finalized homepage layout, styling, and DOM interaction.

--- client_site/js/homepage_logic.js
+++ client_site/js/homepage_logic.js
@@ -1,1 +1,16 @@
-console.log("JS has loaded")
+/*
+Name: Patrick Sullivan
+Date: Feb 8th 2026
+File: homepage_logic.js
+Description: Handles homepage interactivity. Implements CTA
+button click to scroll to contact section by calliing each element by its
+respective Id
+*/
+
+const heroCtaButton = document.getElementById('heroCtaButton');  // Select CTA button by id
+const contactSection = document.getElementById('contactSection'); //Select contact section by id
+
+//State and event when CTA button is clicked, contactSection is scrolled into view
+heroCtaButton.addEventListener("click", () => {
+    contactSection.scrollIntoView({ behavior: "smooth"});
+});
