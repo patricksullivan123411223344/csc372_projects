a62ab5205a7f3b92be7c874bf995c23152c76985
patricksullivan123411223344 <patrick.sullivan@uri.edu>
2026-02-15 18:05:30 -0500

    finished assignment 2

--- client_site/js/contact_page_logic.js
+++ client_site/js/contact_page_logic.js
@@ -1,6 +1,16 @@
+/*
+Name: Patrick Sullivan
+Date: Feb 15, 2026
+File name: contact_page_logic.js
+Description: This page consists of two seperate click events from 
+two seperate buttons next to eachother on the contact page. One button
+scrolls "Email Form" into view, the other scrolls "Session Booker" into
+view.
+*/
+
 /* Declare variables globally to avoid scope problems */
 const questionButton = document.getElementById("toEmailForm");
-const bookButtn = document.getElementById("toCalendlyBooker");
+const bookButton = document.getElementById("toCalendlyBooker");
 const bookSection = document.getElementById("bookingSection");
 const emailSection = document.getElementById("contactSection");
 
